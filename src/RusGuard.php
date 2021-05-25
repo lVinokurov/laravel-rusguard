2d641d81dcbb183cbc88dd5ff9e7b91ade3920f5
Vinokurov Leonid <foks961@yandex.ru>
2021-05-25 15:46:43 +0300

    Change structure

--- src/RusGuard.php
+++ src/RusGuard.php
@@ -2,8 +2,18 @@
 
 namespace lVinokurov\RusGuard;
 
+use ArrayType\ArrayOfguid;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+use ServiceType\Add;
+use ServiceType\Assign;
+use ServiceType\Lock;
+use ServiceType\Set;
+use StructType\AcsKeySaveData;
+use StructType\AddAcsEmployeeGroup;
+use StructType\AssignAcsKeyForEmployee;
+use StructType\LockAcsEmployee;
+use StructType\SetUseEmployeeGroupParentAccessLevel;
 
 class RusGuard
 {
@@ -49,5 +59,107 @@ class RusGuard
         }
 
         return $object;
+    }
+
+    // Employee
+
+    public function createEmployee($group_id, array $args)
+    {
+        $add = new Add($this->options);
+
+        $structure = $this->setStructure(new \StructType\AcsEmployeeSaveData(), $args);
+        $result = $add->AddAcsEmployee(
+            new \StructType\AddAcsEmployee($group_id, $structure)
+        );
+
+        if (!$result) {
+            $this->logger->critical($add->getLastError()->getMessage());
+            throw new \Exception(current($add->getLastError())->getMessage());
+        }
+
+        return $add->getResult()->getAddAcsEmployeeResult();
+    }
+
+    public function lockEmployee(array $args)
+    {
+        $lock = new Lock($this->options);
+        $args['ids'] = new ArrayOfguid(is_array($args['ids']) ? $args['ids'] : [$args['ids']]);
+
+        $structure = $this->setStructure(new LockAcsEmployee(), $args);
+
+        $result = $lock->LockAcsEmployee($structure);
+
+        if (!$result) {
+            $this->logger->critical($lock->getLastError()->getMessage());
+            throw new \Exception(current($lock->getLastError())->getMessage());
+        }
+
+        return true;
+    }
+
+    public function createEmployeePhoto(array $args)
+    {
+        $set = new Set($this->options);
+        $structure = $this->setStructure(new \StructType\SetAcsEmployeePhoto(), $args);
+
+        $result = $set->SetAcsEmployeePhoto($structure);
+
+        if (!$result) {
+            $this->logger->critical($set->getLastError()->getMessage());
+            throw new \Exception(current($set->getLastError())->getMessage());
+        }
+
+        return true;
+    }
+
+    public function addKeyForEmployee(array $args)
+    {
+        $assign = new Assign($this->options);
+
+        if (isset($args['key_data']))
+            $args['key_data'] = $this->setStructure(new AcsKeySaveData(), $args['key_data']);
+
+        $structure = $this->setStructure(new AssignAcsKeyForEmployee(), $args);
+        $result = $assign->AssignAcsKeyForEmployee($structure);
+
+        if (!$result) {
+            $this->logger->critical($assign->getLastError()->getMessage());
+            throw new \Exception(current($assign->getLastError())->getMessage());
+        }
+
+        return true;
+    }
+
+    // Group
+
+    public function createGroup(array $args)
+    {
+        $add = new Add($this->options);
+
+        $structure = $this->setStructure(new AddAcsEmployeeGroup(), $args);
+
+        $result = $add->AddAcsEmployeeGroup($structure);
+
+        if (!$result) {
+            $this->logger->critical($add->getLastError()->getMessage());
+            throw new \Exception(current($add->getLastError())->getMessage());
+        }
+
+        return $add->getResult()->getAddAcsEmployeeGroupResult();
+    }
+
+    public function SetUseEmployeeGroupParentAccessLevel(array $args)
+    {
+        $set = new Set($this->options);
+
+        $structure = $this->setStructure(new SetUseEmployeeGroupParentAccessLevel(), $args);
+        $result = $set->SetUseEmployeeGroupParentAccessLevel($structure);
+
+        if (!$result) {
+            $this->logger->critical($set->getLastError()->getMessage());
+            throw new \Exception(current($set->getLastError())->getMessage());
+        }
+
+        return true;
     }
 }
--- src/RusGuardServiceProvider.php
+++ src/RusGuardServiceProvider.php
@@ -7,24 +7,24 @@ use Illuminate\Support\ServiceProvider;
 
 class RusGuardServiceProvider extends ServiceProvider
 {
-    /**
-     * Register the config for publishing
-     *
-     */
-    public function boot()
-    {
-        $this->publishes([
-            __DIR__.'/../config/rusguard.php' => config_path('rusguard.php'),
-        ]);
-    }
+  /**
+   * Register the config for publishing
+   *
+   */
+  public function boot()
+  {
+    $this->publishes([
+      __DIR__.'/../config/rusguard.php' => config_path('rusguard.php'),
+    ]);
+  }
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->bind('rusguard', RusGuard::class);
-    }
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->app->bind('rusguard', RusGuard::class);
+  }
 }
